e585440577f1ca035c048198ffdd780316e7bb6a
faustomontiel <36174205+faustomontiel@users.noreply.github.com>
2018-02-24 12:56:01 -0300

    parcial 8

--- 9-Parciales/parcial/seis.js
+++ 9-Parciales/parcial/seis.js
@@ -1,4 +1,43 @@
 function Mostrar()
 {
+	var contador=0;
+	var numero;
+	var acumulador=0;
+	var maximo=0;
+	var minimo=0;
+	while(contador<24){
+		contador++;
+		numero=parseInt(prompt("Ingrese numeros ventas de 24 dias habiles"));
+		acumulador=acumulador+numero;
+		
+
+		if(contador==1){
+			numero=maximo;
+		} else{
+			if(maximo<numero){
+				numero=maximo;
+			}else{
+				if(minimo>numero){
+					minimo>numero;
+				}
+			}
+		}
+
+
+		document.write("el importe mayor fue"+maximo);
+		document.write("el importe minimo fue"+minimo);
+
+
+
+
+	}
+
+
+
+
+
+
+
+
 
 }
