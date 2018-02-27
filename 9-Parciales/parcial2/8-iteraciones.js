3fb0bb8c903601d855a5ff090e4b5c585c7f2e73
faustomontiel <36174205+faustomontiel@users.noreply.github.com>
2018-02-26 22:06:37 -0300

    parcial

--- 9-Parciales/parcial2/8-iteraciones.js
+++ 9-Parciales/parcial2/8-iteraciones.js
@@ -1,7 +1,37 @@
 //Debemos lograr mostrar un mensaje al presionar el botón  'MOSTRAR'.
 function Mostrar()
-{
-	alert("Funciona 8-iteraciones");
+{	
+
+	var numero;
+	var contarNum=0;
+	var seguir="";
+	var acumulador=0;
+	var contPar=0;
+
+
+	while(seguir!="no"){
+			numero=parseInt(prompt("Ingrese un numero positivo"));
+
+			contarNum=contarNum+1;
+
+		while(numero<0){
+			numero=parseInt(prompt("Ingrese un numero positivo"));
+		}	
+		acumulador=acumulador+numero;
+
+		seguir=prompt("continuar?");
+
+		if(numero%2==0){
+			contPar=contPar+1;
+		}	
+
+
+
+
+	}
 	
+
+
+	document.write("<br>la cantidad de numeros pares es "+contPar);
 }
 
